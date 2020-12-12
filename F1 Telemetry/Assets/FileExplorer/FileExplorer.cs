59b5be9b91e17f46ef3c807033989663feabb7de
Filip Petersson <zmarfan@hotmail.com>
2020-12-12 22:54:25 +0100

    Basic file explorer

---
+++ F1 Telemetry/Assets/FileExplorer/FileExplorer.cs
@@ -0,0 +1,241 @@
+﻿using UnityEngine;
+using System.IO;
+using System;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileExplorer
+{
+    public class FileExplorer : MonoBehaviour
+    {
+        [Header("Settings")]
+
+        [SerializeField] string _fileNamePre = "File Name: ";
+        [SerializeField] string _fileTypePre = "File Type: ";
+        [SerializeField] string _directoryTypeName = "Directory";
+        [SerializeField] StartDirectories _startFolder;
+
+        [Header("Drop")]
+
+        [SerializeField] Text _fileNameText;
+        [SerializeField] Text _fileTypeText;
+        [SerializeField] InputField _inputfield;
+        [SerializeField] Transform _iconArea;
+        [SerializeField] GameObject _iconPrefab;
+        [SerializeField] FileTypeToSprite[] _fileTypeToSpriteList;
+
+
+        //List of all items currently showing
+        List<Icon> _items = new List<Icon>();
+
+        Dictionary<FileTypes, Sprite> _fileTypeToSprite = new Dictionary<FileTypes, Sprite>();
+        Dictionary<string, FileTypes> _supportedFileTypes = new Dictionary<string, FileTypes>()
+        {
+            { string.Empty, FileTypes.Directory },
+            { ".txt", FileTypes.txt },
+            { ".png", FileTypes.png },
+        };
+
+        string _filePath;
+        bool _isFile = false;
+        string _currentlySelectedItemName = string.Empty;
+
+        private void Awake()
+        {
+            //Initilize dictionary of filetype to sprite for icon retrival
+            for (int i = 0; i < _fileTypeToSpriteList.Length; i++)
+                _fileTypeToSprite.Add(_fileTypeToSpriteList[i].type, _fileTypeToSpriteList[i].sprite);
+
+            Init();
+        }
+
+        /// <summary>
+        /// Search and find from default path
+        /// </summary>
+        public void Init()
+        {
+            GoSpecificPlace((int)_startFolder);
+        }
+
+        /// <summary>
+        /// Called when selecting item -> Sets and display item info
+        /// </summary>
+        void Selected(string name, string extension, string filePath)
+        {
+            _fileNameText.text = _fileNamePre + name;
+            _fileTypeText.text = extension == string.Empty ? _fileTypePre + _directoryTypeName : _fileTypePre + extension;
+        }
+
+        /// <summary>
+        /// Called when double clicking an item
+        /// </summary>
+        void Open(string name, string extension, string filePath)
+        {
+            //It's a directory -> open filepath
+            if (extension == string.Empty)
+            {
+                _inputfield.text = filePath;
+                PressedGo();
+            }
+        }
+
+        /// <summary>
+        /// Loads in files and directories from current filepath
+        /// </summary>
+        public string LoadFilesAndDirectories(string filePath)
+        {
+            DirectoryInfo fileList;
+            try
+            {
+                fileList = new DirectoryInfo(filePath);
+                FileInfo[] files = fileList.GetFiles();
+                DirectoryInfo[] directories = fileList.GetDirectories();
+
+                //Clear file info
+                _fileNameText.text = string.Empty;
+                _fileTypeText.text = string.Empty;
+
+                //Remove all previous icons
+                for (int i = 0; i < _items.Count; i++)
+                {
+                    _items[i].ClickedIcon -= Selected;
+                    _items[i].DoubleClickedIcon -= Open;
+                    Destroy(_items[i].gameObject);
+                }
+                _items.Clear();
+
+                //First show all directories
+                for (int i = 0; i < directories.Length; i++)
+                    SpawnItem(directories[i].Name, directories[i].Extension, directories[i].FullName);
+
+                //Then show all files
+                for (int i = 0; i < files.Length; i++)
+                    SpawnItem(files[i].Name, files[i].Extension, files[i].FullName);
+
+                //The current filepath is valid -> save it
+                return filePath;
+            }
+            catch (Exception e)
+            {
+                //Do nothing -> wrong input
+                Debug.Log(e);
+                //This filepath is invalid, disregard
+                return _filePath;
+            }
+        }
+
+        /// <summary>
+        /// Instantiate file or directory to list and set it's values properly
+        /// </summary>
+        /// <param name="name">File name</param>
+        /// <param name="extension">File type</param>
+        /// <param name="filePath">The full path to this item</param>
+        void SpawnItem(string name, string extension, string filePath)
+        {
+            FileTypes fileType = GetSupportedFileType(extension);
+            GameObject item = Instantiate(_iconPrefab, Vector3.zero, Quaternion.identity, _iconArea) as GameObject;
+            Icon script = item.GetComponent<Icon>();
+            script.SetValues(name, extension, filePath, _fileTypeToSprite[fileType], fileType != FileTypes.Unknown);
+            //Listen to clicked event
+            script.ClickedIcon += Selected;
+            script.DoubleClickedIcon += Open;
+            _items.Add(script);
+        }
+
+        /// <summary>
+        /// Checks if the item being added is supported, returns true if it is and what type of item it is
+        /// </summary>
+        FileTypes GetSupportedFileType(string extenstion)
+        {
+            if (_supportedFileTypes.ContainsKey(extenstion))
+                return _supportedFileTypes[extenstion];
+            return FileTypes.Unknown;
+        }
+
+        /// <summary>
+        /// Move one directory backwards if possible
+        /// </summary>
+        public void Back()
+        {
+            string currentInput = _filePath;
+            StringBuilder builder = new StringBuilder(currentInput);
+            int lastSlashIndex = currentInput.LastIndexOf('\\');
+            //Remove last slash if any
+            if (lastSlashIndex == builder.Length - 1 && lastSlashIndex != -1)
+                builder.Remove(lastSlashIndex, 1);
+
+            int nextSlashEnding = currentInput.LastIndexOf('\\');
+            //There exist a new slash -> remove everything after that
+            if (nextSlashEnding != -1)
+                builder.Remove(nextSlashEnding, builder.Length - nextSlashEnding);
+
+            _inputfield.text = builder.ToString();
+            PressedGo();
+        }
+
+        /// <summary>
+        /// A search is commenced -> load url 
+        /// </summary>
+        public void PressedGo()
+        {
+            string currentFilePath = _inputfield.text;
+            _isFile = false;
+            _filePath = LoadFilesAndDirectories(currentFilePath);
+        }
+
+        /// <summary>
+        /// Search for specific pre chosen folder -> used by quick buttons in interface
+        /// </summary>
+        public void GoSpecificPlace(int startDirectoryIndex)
+        {
+            Environment.SpecialFolder specialFolder;
+
+            switch ((StartDirectories)startDirectoryIndex)
+            {
+                case StartDirectories.dekstop:    specialFolder = Environment.SpecialFolder.DesktopDirectory; break;
+                case StartDirectories.documents:  specialFolder = Environment.SpecialFolder.MyDocuments; break;
+                case StartDirectories.favorites: specialFolder = Environment.SpecialFolder.ProgramFiles; break;
+                default: specialFolder = Environment.SpecialFolder.MyDocuments; break;
+            }
+            _inputfield.text = Environment.GetFolderPath(specialFolder);
+            PressedGo();
+        }
+
+        #region Structs and Enums
+
+        /// <summary>
+        /// Supported filetypes
+        /// </summary>
+        public enum FileTypes
+        {
+            Directory,
+            txt,
+            png,
+            Unknown
+        }
+
+        /// <summary>
+        /// Used to navigate with quick buttons from interface
+        /// </summary>
+        [Serializable]
+        public enum StartDirectories
+        {
+            dekstop,
+            documents,
+            favorites,
+        }
+
+        /// <summary>
+        /// Used to initilize dictionary for filetype sprites
+        /// </summary>
+        [Serializable]
+        public struct FileTypeToSprite
+        {
+            public FileTypes type;
+            public Sprite sprite;
+        }
+
+        #endregion
+    }
+}
---
+++ F1 Telemetry/Assets/FileExplorer/Icon.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace FileExplorer
+{
+    public delegate void PressedIcon(string name, string extension, string filePath);
+
+    public class Icon : MonoBehaviour
+    {
+        [SerializeField, Range(0.01f, 5f)] float _doubleClickWindow = 0.5f;
+        [SerializeField] Color _notSupportedColor;
+        [SerializeField] Text _nameText;
+        [SerializeField] Image _spriteImage;
+        [SerializeField] Image _buttonImage;
+        [SerializeField] Button _button;
+
+        public event PressedIcon ClickedIcon;
+        public event PressedIcon DoubleClickedIcon;
+
+        bool _clickedWillDoubleClick = false;
+        string _name; 
+        string _extension;
+        string _filePath;
+
+        public void SetValues(string name, string extension, string filePath, Sprite sprite, bool supported)
+        {
+            _name = name;
+            _extension = extension;
+            _filePath = filePath;
+
+            _spriteImage.sprite = sprite;
+            _buttonImage.sprite = sprite;
+            _nameText.text = name;
+
+            //Don't allow usage of item if it's not supported
+            if (!supported)
+            {
+                _spriteImage.color = _notSupportedColor;
+                _button.enabled = false;
+                _buttonImage.enabled = false;
+            }
+        }
+
+        /// <summary>
+        /// Gets called when icon is clicked -> Invoke event that it was clicked
+        /// </summary>
+        public void ClickedIconButton()
+        {
+            //Double click -> open
+            if (_clickedWillDoubleClick)
+                DoubleClickedIcon?.Invoke(_name, _extension, _filePath);
+            //Single click -> select
+            else
+            {
+                ClickedIcon?.Invoke(_name, _extension, _filePath);
+                StartCoroutine(Clicked());
+            }
+        }
+
+        /// <summary>
+        /// Keeps track of timing window for double click
+        /// </summary>
+        IEnumerator Clicked()
+        {
+            _clickedWillDoubleClick = true;
+            yield return new WaitForSecondsRealtime(_doubleClickWindow);
+            _clickedWillDoubleClick = false;
+        }
+    }
+}
