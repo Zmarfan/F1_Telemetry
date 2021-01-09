d3ce6b5fc4af738d0730aa53992d9b5dafc78b49
Zmarfan <zmarfan@hotmail.com>
2021-01-10 00:45:47 +0100

    First iteration RGB, HSL, HSV color picker

--- F1 Telemetry/Assets/Script/F1_Unity/UI/ColorPicker/ColorPicker.cs
+++ F1 Telemetry/Assets/Script/F1_Unity/UI/ColorPicker/ColorPicker.cs
@@ -13,16 +13,20 @@ namespace F1_Unity
         [SerializeField] Color _testColor;
         [SerializeField] Image _previewImage;
         [SerializeField] FilterMode _filterMode;
+        [SerializeField] ColorPickerState _colorState = ColorPickerState.HSL;
 
-        [SerializeField] Image _hueImage;
-        [SerializeField] Image _saturationImage;
-        [SerializeField] Image _lightnessImage;
+        [SerializeField] Image _bar0Image;
+        [SerializeField] Image _bar1Image;
+        [SerializeField] Image _bar2Image;
 
-        [SerializeField] Slider _hueSlider;
-        [SerializeField] Slider _saturationSlider;
-        [SerializeField] Slider _lightnessSlider;
+        [SerializeField] Slider _bar0Slider;
+        [SerializeField] Slider _bar1Slider;
+        [SerializeField] Slider _bar2Slider;
+
 
         HSLColor _hslColor;
+        HSVColor _hsvColor;
+        Color _rgbColor;
 
         private void Awake()
         {
@@ -35,10 +39,57 @@ namespace F1_Unity
         public void Init(Color startColor)
         {
             _hslColor = HSLColor.HSLFromRGB(startColor);
-            _hueSlider.value = _hslColor.h;
-            _saturationSlider.value = _hslColor.s;
-            _lightnessSlider.value = _hslColor.l;
+            _hsvColor = HSVColor.HSVFromRGB(startColor);
+            _rgbColor = startColor;
 
+            UpdateSliderValues();
+            UpdateImages();
+            UpdatePreview();
+        }
+
+        /// <summary>
+        /// Sets the value for the sliders
+        /// </summary>
+        void UpdateSliderValues()
+        {
+            SetBarValuesFromState(out float bar_0, out float bar_1, out float bar_2);
+
+            _bar0Slider.value = bar_0;
+            _bar1Slider.value = bar_1;
+            _bar2Slider.value = bar_2;
+        }
+
+        /// <summary>
+        /// Changes state for color picker (HSL -> HSV -> RGB)
+        /// </summary>
+        public void ChangeState()
+        {
+            switch (_colorState)
+            {
+                case ColorPickerState.HSL:
+                    {
+                        _hsvColor = _hslColor.HSVColor();
+                        _rgbColor = _hslColor.RGBColor();
+                        break;
+                    }
+                case ColorPickerState.HSV:
+                    {
+                        _hslColor = _hsvColor.HSLColor();
+                        _rgbColor = _hsvColor.RGBColor();
+                        break;
+                    }
+                case ColorPickerState.RGB:
+                    {
+                        _hslColor = HSLColor.HSLFromRGB(_rgbColor);
+                        _hsvColor = HSVColor.HSVFromRGB(_rgbColor);
+                        break;
+                    }
+                default: throw new System.Exception("This ColorState is not implemented yet: " + _colorState);
+            }
+
+            _colorState = (ColorPickerState)(((int)_colorState + 1) % (int)ColorPickerState.Length);
+
+            UpdateSliderValues();
             UpdateImages();
             UpdatePreview();
         }
@@ -47,16 +98,32 @@ namespace F1_Unity
         /// Update _hslVector from slider value change indicated by which slider
         /// </summary>
         /// <param name="sliderType">What slider</param>
-        public void SliderChange(int sliderType)
+        public void SliderChange()
         {
-            HSLType type = (HSLType)sliderType;
-
-            switch (type)
+            switch (_colorState)
             {
-                case HSLType.Hue:        { _hslColor.h = _hueSlider.value;        break; }
-                case HSLType.Saturation: { _hslColor.s = _saturationSlider.value; break; }
-                case HSLType.lightness:  { _hslColor.l = _lightnessSlider.value;  break; }
-                default: throw new System.Exception("This HSLType is not implemented yet: " + type);
+                case ColorPickerState.HSL:
+                    {
+                        _hslColor.h = _bar0Slider.value;
+                        _hslColor.s = _bar1Slider.value;
+                        _hslColor.l = _bar2Slider.value;
+                        break;
+                    }
+                case ColorPickerState.HSV:
+                    {
+                        _hsvColor.h = _bar0Slider.value;
+                        _hsvColor.s = _bar1Slider.value;
+                        _hsvColor.v = _bar2Slider.value;
+                        break;
+                    }
+                case ColorPickerState.RGB:
+                    {
+                        _rgbColor.r = _bar0Slider.value;
+                        _rgbColor.g = _bar1Slider.value;
+                        _rgbColor.b = _bar2Slider.value;
+                        break;
+                    }
+                default: throw new System.Exception("This ColorState is not implemented yet: " + _colorState);
             }
 
             UpdateImages();
@@ -68,9 +135,9 @@ namespace F1_Unity
         /// </summary>
         void UpdateImages()
         {
-            SetSliderImage(HSLType.Hue, _hueImage);
-            SetSliderImage(HSLType.Saturation, _saturationImage);
-            SetSliderImage(HSLType.lightness, _lightnessImage);
+            SetSliderImage(ColorBarType.Bar_0, _bar0Image);
+            SetSliderImage(ColorBarType.Bar_1, _bar1Image);
+            SetSliderImage(ColorBarType.Bar_2, _bar2Image);
         }
 
         /// <summary>
@@ -78,13 +145,19 @@ namespace F1_Unity
         /// </summary>
         void UpdatePreview()
         {
-            _previewImage.color = _hslColor.RGBColor();
+            switch (_colorState)
+            {
+                case ColorPickerState.HSL: _previewImage.color = _hslColor.RGBColor(); break;
+                case ColorPickerState.HSV: _previewImage.color = _hsvColor.RGBColor(); break;
+                case ColorPickerState.RGB: _previewImage.color = _rgbColor;            break;
+                default: throw new System.Exception("This ColorState is not implemented yet: " + _colorState);
+            }
         }
 
         /// <summary>
         /// Generates a sprite from _hslVector based on hue, saturation or lightness
         /// </summary>
-        void SetSliderImage(HSLType type, Image modifyImage)
+        void SetSliderImage(ColorBarType type, Image modifyImage)
         {
             Texture2D texture = new Texture2D(_textureLength, 1);
             Color[] colorArray = GetColorArray(type);
@@ -99,32 +172,103 @@ namespace F1_Unity
         /// <summary>
         /// Gets a colorArray _textureLength x 1 based on HSLType
         /// </summary>
-        Color[] GetColorArray(HSLType type)
+        Color[] GetColorArray(ColorBarType type)
         {
             Color[] colorArray = new Color[_textureLength];
 
             for (int i = 0; i < _textureLength; i++)
             {
                 float point = i / (float)_textureLength;
-                Color color;
-                switch (type)
-                {
-                    case HSLType.Hue:        { color = HSLColor.RGBColor(point, _hslColor.s, _hslColor.l); break; }
-                    case HSLType.Saturation: { color = HSLColor.RGBColor(_hslColor.h, point, _hslColor.l); break; }
-                    case HSLType.lightness:  { color = HSLColor.RGBColor(_hslColor.h, _hslColor.s, point); break; }
-                    default: throw new System.Exception("This HSLType is not implemented yet: " + type);
-                }
+                Color color = GetColor(type, point);
                 colorArray[i] = color;
             }
 
             return colorArray;
         }
 
-        enum HSLType
+        /// <summary>
+        /// Gets RGB color depending on ColorPickerState and for which bar it interpolates
+        /// </summary>
+        /// <param name="type">Which bar?</param>
+        /// <param name="point">point value to replace that bars stored value</param>
+        /// <returns></returns>
+        Color GetColor(ColorBarType type, float point)
         {
-            Hue,
-            Saturation,
-            lightness
+            switch (type)
+            {
+                case ColorBarType.Bar_0:
+                    {
+                        switch (_colorState)
+                        {
+                            case ColorPickerState.HSL: return HSLColor.RGBColor(point, _hslColor.s, _hslColor.l);
+                            case ColorPickerState.HSV: return HSVColor.RGBColor(point, _hsvColor.s, _hsvColor.v);
+                            case ColorPickerState.RGB: return new Color(point, _rgbColor.g, _rgbColor.b);
+                            default: throw new System.Exception("This ColorState is not implemented yet: " + _colorState);
+                        }
+                    }
+                case ColorBarType.Bar_1:
+                    {
+                        switch (_colorState)
+                        {
+                            case ColorPickerState.HSL: return HSLColor.RGBColor(_hslColor.h, point, _hslColor.l);
+                            case ColorPickerState.HSV: return HSVColor.RGBColor(_hsvColor.h, point, _hsvColor.v);
+                            case ColorPickerState.RGB: return new Color(_rgbColor.r, point, _rgbColor.b);
+                            default: throw new System.Exception("This ColorState is not implemented yet: " + _colorState);
+                        }
+                    }
+                case ColorBarType.Bar_2:
+                    {
+                        switch (_colorState)
+                        {
+                            case ColorPickerState.HSL: return HSLColor.RGBColor(_hslColor.h, _hslColor.s, point);
+                            case ColorPickerState.HSV: return HSVColor.RGBColor(_hsvColor.h, _hsvColor.s, point);
+                            case ColorPickerState.RGB: return new Color(_rgbColor.r, _rgbColor.g, point);
+                            default: throw new System.Exception("This ColorState is not implemented yet: " + _colorState);
+                        }
+                    }
+                default: throw new System.Exception("This HSLType is not implemented yet: " + type);
+            }
+        }
+
+        /// <summary>
+        /// Sets bar values from hsl, hsv or rgb depending on state
+        /// </summary>
+        public void SetBarValuesFromState(out float bar_0, out float bar_1, out float bar_2)
+        {
+            switch (_colorState)
+            {
+                case ColorPickerState.HSL: { bar_0 = _hslColor.h; bar_1 = _hslColor.s; bar_2 = _hslColor.l; break; }
+                case ColorPickerState.HSV: { bar_0 = _hsvColor.h; bar_1 = _hsvColor.s; bar_2 = _hsvColor.v; break; }
+                case ColorPickerState.RGB: { bar_0 = _rgbColor.r; bar_1 = _rgbColor.g; bar_2 = _rgbColor.b; break; }
+                default: throw new System.Exception("This ColorState is not implemented yet: " + _colorState);
+            }
+        }
+
+        enum ColorPickerState
+        {
+            HSL,
+            HSV,
+            RGB,
+            Length
+        }
+
+        /// <summary>
+        /// What bar are this refering to in HSL, HSV and RGB
+        /// </summary>
+        enum ColorBarType
+        {
+            /// <summary>
+            /// Hue or Red
+            /// </summary>
+            Bar_0,
+            /// <summary>
+            /// Saturation or Green
+            /// </summary>
+            Bar_1,
+            /// <summary>
+            /// Lightness, value or blue
+            /// </summary>
+            Bar_2
         }
     }
 }
--- F1 Telemetry/Assets/Script/F1_Unity/UI/ColorPicker/HSLColor.cs
+++ F1 Telemetry/Assets/Script/F1_Unity/UI/ColorPicker/HSLColor.cs
@@ -1,5 +1,68 @@
 ﻿using UnityEngine;
 
+[System.Serializable]
+public struct HSVColor
+{
+    public float h;
+    public float s;
+    public float v;
+
+    public HSVColor(float h, float s, float v)
+    {
+        this.h = h;
+        this.s = s;
+        this.v = v;
+    }
+
+    /// <summary>
+    /// Return color as a RGB representation
+    /// </summary>
+    /// <returns></returns>
+    public Color RGBColor()
+    {
+        return Color.HSVToRGB(h, s, v);
+    }
+
+    /// <summary>
+    /// Return color as HSL Representation
+    /// </summary>
+    public HSLColor HSLColor()
+    {
+        HSLColor hslColor = new HSLColor(h, s, v);
+        hslColor.l = (2 - hslColor.s) * v / 2;
+
+        if (hslColor.l != 0)
+        {
+            if (hslColor.l == 1)
+                hslColor.s = 0;
+            else if (hslColor.l < 0.5f)
+                hslColor.s = hslColor.s * v / (hslColor.l * 2);
+            else
+                hslColor.s = hslColor.s * v / (2 - hslColor.l * 2);
+        }
+
+        return hslColor;
+    }
+
+    /// <summary>
+    /// Creates a RGB color from HSV variables
+    /// </summary>
+    /// <param name="h">Hue</param>
+    /// <param name="s">Saturation</param>
+    /// <param name="v">Value</param>
+    public static Color RGBColor(float h, float s, float v)
+    {
+        return Color.HSVToRGB(h, s, v);
+    }
+
+    public static HSVColor HSVFromRGB(Color color)
+    {
+        Color.RGBToHSV(color, out float h, out float s, out float v);
+        return new HSVColor(h, s, v);
+    }
+}
+
+[System.Serializable]
 public struct HSLColor
 {
     public float h;
@@ -18,6 +81,16 @@ public struct HSLColor
     /// </summary>
     public Color RGBColor()
     {
+        HSVColor hsvColor = HSVColor();
+        return Color.HSVToRGB(hsvColor.h, hsvColor.s, hsvColor.v);
+    }
+
+    /// <summary>
+    /// Converts this HSL color to a HSV Color
+    /// </summary>
+    /// <returns></returns>
+    public HSVColor HSVColor()
+    {
         //Modify hsl vector to easier create HSV vector
         float modH = h;
         float modL = l * 2;
@@ -25,10 +98,10 @@ public struct HSLColor
 
         //Generate a HSV vector from HSL vector -> takes less code and can utilize Unity function 
         float hsvH = modH;
-        float hsvS = (2 * modS) / (modL + modS);
+        float hsvS = 2 * modS / (modL + modS != 0 ? modL + modS : 1);
         float hsvV = (modL + modS) / 2;
 
-        return Color.HSVToRGB(hsvH, hsvS, hsvV);
+        return new HSVColor(hsvH, hsvS, hsvV);
     }
 
     /// <summary>
