e79d78c5fce512bc52b04718a05b604a0dd02503
Zmarfan <zmarfan@hotmail.com>
2021-01-14 14:32:18 +0100

    Fixed Team enums

--- F1 Telemetry/Assets/Script/F1_Data_Management/Enums/Team.cs
+++ F1 Telemetry/Assets/Script/F1_Data_Management/Enums/Team.cs
@@ -2,62 +2,59 @@
 {
     public enum Team
     {
-        Mercedes_AMG_Petronas,
-        Ferrari,
-        Red_Bull_Racing,
-        Williams,
-        BWT_Racing_Point,
-        Renault,
-        AlphaTauri,
-        Haas,
-        McLaren,
-        Alfa_Romeo_Racing,
-        McLaren_1988,
-        McLaren_1991,
-        Williams_1992,
-        Ferrari_1995,
-        Williams_1996,
-        McLaren_1998,
-        Ferrari_2002,
-        Ferrari_2004,
-        Renault_2006,
-        Ferrari_2007,
-        McLaren_2008,
-        Red_Bull_2010,
-        Ferrari_1976,
-        ART_Grand_Prix,
-        Campos_Vexatec_Racing,
-        Carlin,
-        Charouz_Racing_System,
-        DAMS,
-        Virtuosi_Racing,
-        MP_Motorsport,
-        Pertamina,
-        McLaren_1990,
-        Trident,
-        BWT_Arden,
-        McLaren_1976,
-        Lotus_1972,
-        Ferrari_1979,
-        McLaren_1982,
-        Williams_2003,
-        Brawn_2009,
-        Lotus_1978,
-        F1_Generic_car,
-        Art_GP_19,
-        Campos_19,
-        Carlin_19,
-        Sauber_Junior_Charouz_19,
-        Dams_19,
-        Uni_Virtuosi_19,
-        MP_Motorsport_19,
-        Prema_19,
-        Trident_19,
-        Arden_19,
-        Benetton_1994 = 53,
-        Benetton_1995,
-        Ferrari_2000,
-        Jordan_1991,
-        My_Team_Or_Unknown = 255
+        Mercedes_AMG_Petronas,          //Works
+        Ferrari,                        //Works
+        Red_Bull_Racing,                //Works
+        Williams,                       //Works
+        BWT_Racing_Point,               //Works
+        Renault,                        //Works
+        AlphaTauri,                     //Works
+        Haas,                           //Works
+        McLaren,                        //Works
+        Alfa_Romeo_Racing,              //Works
+
+        McLaren_1991 = 11,              //Works
+        Williams_1992,                  //Works
+        Williams_1996 = 14,             //Works
+        McLaren_1998,                   //Works 
+        Ferrari_2004 = 17,              //Works              
+        Renault_2006,                   //Works           
+        Ferrari_2007,                   //Works     
+        McLaren_2008,                   //Works
+        Red_Bull_2010,                  //Works      
+        McLaren_1990 = 31,              //Works
+        Williams_2003 = 38,             //Works
+        Brawn_2009,                     //Works
+
+        Multiplayer_Car = 41,           //Works
+
+        ART_Grand_Prix_2019 = 42,       //Works
+        Campos_Racing_2019,             //Works
+        Carlin_2019,                    //Works
+        Sauber_Junior_Team_2019,        //Works
+        DAMS_2019,                      //Works
+        Uni_Virtuosi_Racing_2019,       //Works
+        MP_Motorsport_2019,             //Works
+        PREMA_Racing_2019,              //Works
+        Trident_2019,                   //Works
+        BWT_Arden_2019,                 //Works
+
+        Ferrari_1990 = 63,              //Works
+        McLaren_2010,                   //Works        
+        Ferrari_2010,                   //Works              
+
+        ART_Grand_Prix = 70,            //Works
+        Campos_Racing,                  //Works
+        Carlin,                         //Works
+        Charouz_Racing_System,          //Works
+        DAMS,                           //Works
+        Uni_Virtuosi_Racing,            //Works
+        MP_Motorsport,                  //Works
+        PREMA_Racing,                   //Works
+        Trident,                        //Works
+        BWT_HWA_Racelab,                //Works
+        Hitech_Grand_Prix,              //Works
+
+        My_Team_Or_Unknown = 85         //Works
     }
 }
