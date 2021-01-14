9ae3eb2a6a0c78bedb19830d53a65c151dce9efb
Zmarfan <zmarfan@hotmail.com>
2021-01-14 14:31:57 +0100

    Fixed Visual and Actual tyre enums

--- F1 Telemetry/Assets/Script/F1_Data_Management/Enums/ActualTyreCompound.cs
+++ F1 Telemetry/Assets/Script/F1_Data_Management/Enums/ActualTyreCompound.cs
@@ -2,23 +2,27 @@
 {
     public enum ActualTyreCompound
     {
-        //F1 Modern Wet
+         //F1 Modern Wet
         Intermediate = 7,
         Wet,
         //F1 Classic
         Classic_Dry,
         Classic_Wet,
-        //F2
-        F2_Super_Soft,
+
+        //F2 dry
+        F2_Super_Soft= 11,
         F2_Soft,
         F2_Medium,
         F2_Hard,
-        F2_Wet,
+
+        //F2 Wet
+        F2_Wet = 15,
+
         //F1 Modern dry
-        C5,
+        C5 = 16,
         C4,
         C3,
         C2,
-        C1
+        C1,
     }
 }
--- F1 Telemetry/Assets/Script/F1_Data_Management/Enums/VisualTyreCompound.cs
+++ F1 Telemetry/Assets/Script/F1_Data_Management/Enums/VisualTyreCompound.cs
@@ -8,15 +8,28 @@
         //F1 Classic
         Classic_Dry,
         Classic_Wet,
-        //F2
-        F2_Super_Soft,
+
+        //F2 2019 Wet
+        F2_Wet_2019 = 14,
+
+        //F2 2020 Wet
+        F2_Wet = 15,
+
+        //F1 Modern dry
+        Soft = 16,
+        Medium,
+        Hard,
+
+        //F2 2019 dry
+        F2_Super_Soft_2019 = 19,
+        F2_Soft_2019,
+        F2_Medium_2019,
+        F2_Hard_2019,
+
+        //F2 2020 dry
+        F2_Super_Soft = 23,
         F2_Soft,
         F2_Medium,
         F2_Hard,
-        F2_Wet,
-        //F1 Modern dry
-        Soft,
-        Medium,
-        Hard
     }
 }
